2e55f7cdff6e2ce8e6e0bd79885c997a5430532e
Evan Chang <toosyou70207@gmail.com>
2015-10-27 04:00:41 +0800

    have to sleep, something wrong at line.136 or so . It catched a errno0. WIP

--- MyShell/main.cpp
+++ MyShell/main.cpp
@@ -66,7 +66,6 @@ void shell_command_parser(string &input,command &command_line){
     string buffer;
 
     command_segment tmp_seg;
-
     while(iss >> buffer){
         if( buffer[buffer.size()-1] == '|' ){
             if(buffer.size() != 0){
@@ -106,28 +105,88 @@ int shell_exec_builtin(command_segment &segment){
 }
 
 int shell_exec_segment(command_segment &segment, int in_fd, int out_fd, int mode, int pgid){
-    //cout << segment.args[0] <<endl;
+    for(int i=0;i<segment.args.size();++i){
+        cout << segment.args[i] << " ";
+    }cout <<endl;
 
     if(shell_exec_builtin(segment))
         return 0;
 
+    pid_t childpid = fork();
 
+    if(childpid == 0){//child
+        if(in_fd != 0 && dup2(in_fd,STDIN_FILENO) == -1){
+            exit(errno);
+        }
+
+        if(out_fd != 1 && dup2(out_fd,STDOUT_FILENO) == -1){
+            exit(errno);
+        }
+
+        char **args = new char*[segment.args.size()+1];
+        for(int i=0;i<segment.args.size();++i){
+            args[i] = new char[ segment.args.size()+2 ];
+            strcpy(args[i],segment.args[i].c_str());
+        }
+        args[ segment.args.size() ] = NULL;
+
+        execvp(segment.args[0].c_str(),args);
+        exit(errno);
+    }
+    else{//parent
+        cout << "Command executed by pid=" << childpid <<endl;
+        if(mode == FOREGROUND_EXECUTION){
+            int state = 0;
+            waitpid(childpid,&state,0);
+            if(WIFEXITED(state)){
+                cout << strerror(WEXITSTATUS(state)) <<endl;
+                return -1;
+            }
+            return 0;
+        }
+    }
 
     return 0;
 }
 
 int shell_exec_command(command &command_line){
-    while(!command_line.segment.empty()){
-        int fd[2];
-        pipe(fd);
+    list<int> fds;
+    fds.push_back(0);
+    for(int i=0;i<command_line.segment.size()-1;++i){
+        int tmp_fd[2];
+        pipe(tmp_fd);
+        fds.push_back(tmp_fd[0]);
+        fds.push_back(tmp_fd[1]);
+    }
+    fds.push_back(1);
 
-        if(shell_exec_segment(*command_line.segment.begin(),fd[0],fd[1],command_line.mode,0 ) != 0){
+    while(!command_line.segment.empty()){
+
+        list<int>::iterator tmp_it = fds.begin();
+        int fd0 = *fds.begin();
+        tmp_it++;
+        int fd1 = *tmp_it;
+
+        if(shell_exec_segment(*command_line.segment.begin(),fd0,fd1,command_line.mode,0 ) != 0){
             command_line.clear();
+            while(!fds.empty()){
+                if(fd0 > 1)
+                    close(fd0);
+                fds.pop_front();
+            }
             return 0;
         }
         else{
             command_line.segment.pop_front();
         }
+        if(fd0 > 1)
+            close(fd0);
+        fds.pop_front();
+    }
+    while(!fds.empty()){
+        if((*fds.begin()) > 1)
+            close((*fds.begin()));
+        fds.pop_front();
     }
     return 0;
 }
